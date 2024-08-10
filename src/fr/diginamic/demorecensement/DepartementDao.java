15a50899391f016a4c9654ce120b1f51f998ece7
NGOM-cheikh <mourtalla211@gmail.com>
2024-08-10 12:52:05 +0200

    Initial commit

---
+++ src/fr/diginamic/demorecensement/DepartementDao.java
@@ -0,0 +1,2 @@
+package fr.diginamic.demorecensement;public class DepartementDao {
+}
---
+++ src/fr/diginamic/demorecensement/IntegrationRecensement.java
@@ -0,0 +1,15 @@
+
+
+package fr.diginamic.demorecensement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoRecensementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
